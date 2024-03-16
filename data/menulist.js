d7f9b3bf6c24a7d0cf0b8f2de635c69fd3a46619
PaoloDno <cabrerapaolodino@gmail.com>
2024-03-17 02:27:55 +0800

    Updates

---
+++ data/menulist.js
@@ -0,0 +1,85 @@
+const menulist = [
+  {
+    "id": 0,
+    "name": "Espresso",
+    "category": "Drinks",
+    "sub_category": "Coffee",
+    "price": 30,
+    "img": "../../public/images/menu_pic/menu8.jpg",
+    "desc": "A single shot of strong black coffee made by forcing steam through finely-ground coffee beans."
+  },
+  {
+    "id": 1,
+    "name": "Latte",
+    "category": "Drinks",
+    "sub_category": "Coffee",
+    "price": 60,
+    "img": "../../public/images/menu_pic/menu.jpg",
+    "desc": "A coffee drink made with espresso and steamed milk."
+  },
+  {
+    "id": 2,
+    "name": "Cappuccino",
+    "category": "Drinks",
+    "sub_category": "Coffee",
+    "price": 65,
+    "img": "../../public/images/menu_pic/menu2.jpg",
+    "desc": "An Italian coffee drink that is traditionally prepared with equal parts double espresso, steamed milk, and milk foam."
+  },
+  {
+    "id": 3,
+    "name": "Iced Tea",
+    "category": "Drinks",
+    "sub_category": "Tea",
+    "price": 45,
+    "img": "../../public/images/menu_pic/menu5.jpg",
+    "desc": "A chilled tea beverage served over ice cubes, often sweetened and sometimes flavored."
+  },
+  {
+    "id": 4,
+    "name": "Club Sandwich",
+    "category": "Food",
+    "sub_category": "Sandwiches",
+    "price": 40,
+    "img": "../../public/images/menu_pic/menu5.jpg",
+    "desc": "A delicious sandwich made with sliced chicken or turkey, bacon, lettuce, tomato, and mayonnaise."
+  },
+  {
+    "id": 5,
+    "name": "Strawberry Cheesecake",
+    "category": "Desserts",
+    "sub_category": "Cakes",
+    "price": 75,
+    "img": "../../public/images/menu_pic/menu5.jpg",
+    "desc": "A creamy cheesecake with a graham cracker crust, topped with fresh strawberries."
+  },
+  {
+    "id": 6,
+    "name": "French Fries",
+    "category": "Sides",
+    "sub_category": "Appetizers",
+    "price": 20,
+    "img": "../../public/images/menu_pic/menu5.jpg",
+    "desc": "Deep-fried potato strips seasoned with salt, served hot."
+  },
+  {
+    "id": 7,
+    "name": "Donuts",
+    "category": "Desserts",
+    "sub_category": "Pastries",
+    "price": 30,
+    "img": "../../public/images/menu_pic/menu7.jpg",
+    "desc": "Sweet, deep-fried dough pastries usually shaped like a ring or filled with cream or jam."
+  },
+  {
+    "id": 8,
+    "name": "Brownies",
+    "category": "Desserts",
+    "sub_category": "Pastries",
+    "price": 80,
+    "img": "../../public/images/menu_pic/menu6.jpg",
+    "desc": "Overloaded with chocolate goodness and nuts on top."
+  }
+]
+
+export default menulist
---
+++ data/sliderimages.js
@@ -0,0 +1,56 @@
+const sliderimage = [
+  
+  {
+    id: 1,
+    background_img: "../../public/images/heroslider/coffeshopinterior.jpg",
+    bg_card_images: "../../public/images/heroslider/coffeeandtable.jpg",
+    header_text: "WELCOME TO Cafeko!",
+    header_text_card: "Welcome to Cafeko Experience greatness in a cup",
+    p_text: "Experience greatness in a cup.",
+    link: "#non-slider-container",
+    button_text: "SEE MORE..."
+  },
+  {
+    id: 2,
+    background_img: "../../public/images/heroslider/coffeshopinterior.jpg",
+    bg_card_images: "../../public/images/heroslider/discount.jpg",
+    header_text: "Discount on the Web.",
+    header_text_card: "Welcome to Cafeko",
+    p_text: "Indulge Yourself with Our Brown and Black Coffee Selection at Cafeko",
+    link: "/order",
+    button_text: "ORDER NOW"
+
+  },
+  {
+    id: 3,
+    background_img: "../../public/images/heroslider/coffeshopinterior.jpg",
+    bg_card_images: "../../public/images/heroslider/t-shirts.jpg",
+    header_text: "Elevate your lifestyle.",
+    header_text_card: "Welcome to Cafeko",
+    p_text: "Cafeko is more than just a coffee experience. It's a lifestyle—an aesthetic that celebrates the beauty of simple pleasures and the joy of everyday rituals",
+    link: "/",
+    button_text: "GET GOODIES!"
+  },
+  {
+    id: 4,
+    background_img: "../../public/images/heroslider/coffeshopinterior.jpg",
+    bg_card_images: "../../public/images/heroslider/joinus.jpg",
+    header_text: "Discover. Connect. Brew",
+    header_text_card: "Welcome to Cafeko",
+    p_text: "We're on a mission to redefine brown coffee. Join us to discover our passion, connect with our community, and brew something extraordinary together.",
+    link: "/",
+    button_text: "JOIN US"
+  },
+  {
+    id: 5,
+    background_img: "../../public/images/heroslider/coffeshopinterior.jpg",
+    bg_card_images: "../../public/images/heroslider/signup.jpg",
+    header_text: "Sign Up! for Rewards",
+    header_text_card: "Welcome to Cafeko",
+    p_text: "Get exclusive Seasonal Rewards and Promos, and many more! using the Web or App",
+    link: "/",
+    button_text: "SIGN UP"
+  }
+]
+
+export default sliderimage;
