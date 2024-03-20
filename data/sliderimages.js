ba4d2b9df6b8d53927c122fd01d1d91826427d34
PaoloDno <cabrerapaolodino@gmail.com>
2024-03-20 23:29:19 +0800

    Updates

--- data/sliderimages.js
+++ data/sliderimages.js
@@ -7,7 +7,7 @@ const sliderimage = [
     header_text: "Best Espresso in Town",
     header_text_card: "Try our bestseller all black and darker than black, Espresso",
     p_text: "Experience Greatness in a Cup",
-    link: "/order",
+    link: "/cafeshopwebsite/order",
     button_text: "GET IT NOW"
   },
   {
@@ -17,7 +17,7 @@ const sliderimage = [
     header_text: "We got the fresh beans and more!",
     header_text_card: "Welcome to Cafeko",
     p_text: "Indulge Yourself with Our Brown and Black Coffee Selection at Cafeko, and we have snacks too",
-    link: "/order",
+    link: "/cafeshopwebsite/order",
     button_text: "ORDER NOW"
   },
   {
@@ -27,7 +27,7 @@ const sliderimage = [
     header_text: "Elevate your Lifestyle.",
     header_text_card: "Welcome to Cafeko",
     p_text: "Cafeko is more than just a coffee experience. It's a lifestyle—an aesthetic that celebrates the beauty of simple pleasures and the joy of everyday rituals",
-    link: "/",
+    link: "/cafeshopwebsite/",
     button_text: "GET GOODIES!"
   },
   {
@@ -37,7 +37,7 @@ const sliderimage = [
     header_text: "Discover. Connect. Brew",
     header_text_card: "Welcome to Cafeko",
     p_text: "We're on a mission to redefine brown coffee. Join us to discover our passion, connect with our community, and brew something extraordinary together.",
-    link: "/",
+    link: "/cafeshopwebsite/",
     button_text: "JOIN US"
   },
   {
@@ -47,7 +47,7 @@ const sliderimage = [
     header_text: "Sign Up! for Rewards",
     header_text_card: "Welcome to Cafeko",
     p_text: "Get exclusive Seasonal Rewards and Promos, and many more! using the Web or App",
-    link: "/",
+    link: "/cafeshopwebsite/",
     button_text: "SIGN UP"
   }
 ]
